2cdb633460dedb35cc61e1d0cfe11d3ba7baf3ae
adetimmy <ogunleademola@gmail.com>
2022-12-03 05:57:07 +0100

    readjusting some parameters

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".menu")
 const closeMenu = document.querySelector(".overlay svg")
 const hi = document.querySelector(".come h1")
 window.addEventListener('scroll', () => {
-    document.querySelector('header').classList.toggle("window-scroll", window.scrollY>300)
+    document.querySelector('header').classList.toggle("window-scroll", window.scrollY>0)
     })
 
 menu.addEventListener("click", () =>{
